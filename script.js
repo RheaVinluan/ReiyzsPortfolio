8cf9a660e2cc5f59977ea6d6240ad07f44124d2a
RheaVinluan <vinluan.rheaclaireangela.alejo@gmail.com>
2026-06-15 20:58:12 +0800

    add vercel analytics

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navMenu = document.querySelector(".menu-right");
 burger?.addEventListener("click", () => {
   navMenu?.classList.toggle("active");
 });
+
+//vercel analytics
+
+import { inject } from '@vercel/analytics';
+
+inject();
