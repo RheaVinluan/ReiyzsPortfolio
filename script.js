b77f4d79cfd6b302ec64321b8fe0e1fd34a01b2e
RheaVinluan <vinluan.rheaclaireangela.alejo@gmail.com>
2026-06-17 15:04:31 +0800

    fixed burger icon

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const burger = document.querySelector(".burger");
 const navMenu = document.querySelector(".menu-right");
 
-burger?.addEventListener("click", () => {
-  navMenu?.classList.toggle("active");
+burger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
 });
-
-//vercel analytics
-
-import { inject } from '@vercel/analytics';
-
-inject();
